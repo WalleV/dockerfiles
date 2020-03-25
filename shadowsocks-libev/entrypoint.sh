52a99d30516ed2c6eff262ecda5cfbae0a9da686
Walle <1@qq.com>
2020-03-25 07:14:33 +0000

    add ss and v2ray-plugin

--- shadowsocks-libev/entrypoint.sh
+++ shadowsocks-libev/entrypoint.sh
@@ -1,34 +1,31 @@
 #!/bin/sh
 
-
 if [ "$MODULE" = "ss-local" ];then
-
     $MODULE -s $SERVER_ADDR \
-             -p $SERVER_PORT \
-             -k $PASSWORD \
-             -b $LOCAL_ADDR \
-             -l $LOCAL_PORT \
-             -m $METHOD \
-             -t $TIMEOUT \
-             --fast-open  \
-             -a $USER \
-             --plugin obfs-local \
-             --plugin-opts "obfs=http;obfs-host=www.bing.com" \
-             $ARGS
+            -p $SERVER_PORT \
+            -k $PASSWORD \
+            -b $LOCAL_ADDR \
+            -l $LOCAL_PORT \
+            -m $METHOD \
+            -t $TIMEOUT \
+            --fast-open  \
+            -a $USER \
+            --plugin v2ray-plugin \
+            --plugin-opts "host=intl.aliyun.com" \
+            $ARGS
 
 else
-
-   $MODULE -s $SERVER_ADDR \
-                -p $SERVER_PORT \
-                -k ${PASSWORD:-$(hostname)} \
-                -m $METHOD \
-                -t $TIMEOUT \
-                --fast-open \
-                -d $DNS_ADDR \
-                -d $DNS_ADDR_2 \
-                -u \
-                -a $USER \
-                --plugin obfs-server \
-                --plugin-opts "obfs=http" \
-                $ARGS
+   $MODULE  -s $SERVER_ADDR \
+            -p $SERVER_PORT \
+            -k ${PASSWORD:-$(hostname)} \
+            -m $METHOD \
+            -t $TIMEOUT \
+            --fast-open \
+            -d $DNS_ADDR \
+            -d $DNS_ADDR_2 \
+            -u \
+            -a $USER \
+            --plugin v2ray-plugin \
+            --plugin-opts "server;host=intl.aliyun.com" \
+            $ARGS
 fi
